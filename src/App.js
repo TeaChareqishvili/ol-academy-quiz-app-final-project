5fc56ce6962194c57f609c365405346a51f516aa
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-05-30 20:36:06 +0400

    home page style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
-
-
+import { Routes, Route } from "react-router-dom";
+import { Home } from "./components/Home";
 
 function App() {
   return (
     <div className="App">
-     
+       <Routes>
+        <Route path="/" element={<Home />} />
+        {/* <Route  path="/quiztest" element={<QuizTest/>} /> */}
+        <Route/>
+      </Routes>
     </div>
   );
 }
---
+++ src/components/Home.jsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom";
+import "./HomeStyle.scss";
+
+function Home() {
+  return (
+    <div className="home-page">
+      <div>
+        <h1>Home Page</h1>
+        <nav>
+          <NavLink to="/QuizTest">
+            <button className="start-quiz">Start Quiz</button>
+          </NavLink>
+        </nav>
+      </div>
+      <div>
+        <img src="quiz.jpeg 20-30-48-133.jpeg" alt="quiz" />
+      </div>
+    </div>
+  );
+}
+
+export { Home };
