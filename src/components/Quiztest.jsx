1ae9e194f9affd49cc6242e953ff2b1699dd08cc
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-05-31 11:12:04 +0400

    fetch data and save in local storage

--- src/components/Quiztest.jsx
+++ src/components/Quiztest.jsx
@@ -1,27 +1,63 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from "react";
 
+function QuizTest() {
+  const [quiz, setQuiz] = useState();
+  const data =
+    "http://my-json-server.typicode.com/DanielBarbakadze/Advanced-JS-and-React-Basics/db";
+  const EXPIRATION_TIME = 10 * 60 * 1000;
 
-function QuizTest(){
+  useEffect(() => {
+    const fetchDataFromLocalStorage = () => {
+      const storedData = localStorage.getItem("quizData");
+      const storedTimestamp = localStorage.getItem("quizDataTimestamp");
+      const currentTime = Date.now();
+      console.log("take from local storage");
 
-    const [quiz, setQuiz] = useState();
-    const data = 'http://my-json-server.typicode.com/DanielBarbakadze/Advanced-JS-and-React-Basics/db'
+      if (
+        storedData &&
+        storedTimestamp &&
+        currentTime - storedTimestamp < EXPIRATION_TIME
+      ) {
+        return JSON.parse(storedData);
+      }
 
-    useEffect(()=>{
+      return null;
+    };
 
-         fetch(data)
-        .then((data)=>data.json())
-        .then((json)=>setQuiz(json))
-       
-    })
+    const fetchData = async () => {
+      const storedData = fetchDataFromLocalStorage();
 
+      if (storedData) {
+        setQuiz(storedData);
+        console.log("setquiz");
+      } else {
+        try {
+          const response = await fetch(data);
+          const jsonData = await response.json();
+          setQuiz(jsonData);
+          console.log("getfetch");
 
+          saveDataToLocalStorage(jsonData);
+        } catch (error) {
+          console.error("Error fetching data:", error);
+        }
+      }
+    };
+    fetchData();
+  }, [EXPIRATION_TIME]);
 
+  const saveDataToLocalStorage = (data) => {
+    localStorage.setItem("quizData", JSON.stringify(data));
+    localStorage.setItem("quizDataTimestamp", Date.now().toString());
+  };
 
-    return(
-        <div>
-           {quiz ? (<h2>hi quiz</h2>) : (<h2>it is loading</h2>)}
-        </div>
+  return (
+  <div>
+    {quiz ? 
+    <h2>hi quiz</h2> 
+    : <h2>it is loading</h2>}
+    </div>
     )
 }
 
-export {QuizTest}
+export { QuizTest };
