d1e50af28cff0ad021dd36b8c573ba7f5e1fc2cf
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-06-03 21:01:52 +0400

    progressbar colors

--- src/components/ProgressBarLoad.jsx
+++ src/components/ProgressBarLoad.jsx
@@ -2,11 +2,16 @@ import "./ProgressBarStyle.scss";
 
 function ProgressBarLoad({ progress }) {
   const getColor = () => {
-    if (progress < 40) {
-      console.log("color");
-      return "red";
-    } else {
-      return "green";
+    if (progress < 30) {
+      return "#FF0000";
+    } else if (progress < 50) {
+      return "#FFA500";
+    } else if (progress < 70) {
+      return "#9ACD32";
+    } else if (progress < 80) {
+      return "#008000";
+    } else if (progress >= 80) {
+      return "#044704";
     }
   };
 
