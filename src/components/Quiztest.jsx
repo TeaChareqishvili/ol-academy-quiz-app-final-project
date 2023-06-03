9ed99416eb2813c0868aef2816e816ba1b8b4ede
TeaChareqishvili <teachareqishvili89@gmail.com>
2023-06-03 18:26:59 +0400

    quiz render

--- src/components/Quiztest.jsx
+++ src/components/Quiztest.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
-import {useFetchData} from "../Hooks/useFetchData"
-import {Loader} from "./Loader"
+import React, { useState } from "react";
+import { useFetchData } from "../Hooks/useFetchData";
+import { Loader } from "./Loader";
 
 function QuizTest() {
   const { quiz } = useFetchData();
   const [currentQuestion, setCurrentQuestion] = useState(0);
-  const [selectedValue, setSelectedValue] = useState('');
+  const [selectedValue, setSelectedValue] = useState("");
 
   const handleNextQuestion = () => {
     setCurrentQuestion((prevQuestion) => prevQuestion + 1);
@@ -19,34 +19,46 @@ function QuizTest() {
           {currentQuestion < quiz.questions.length ? (
             <div key={quiz.questions[currentQuestion].id}>
               <p>{quiz.questions[currentQuestion].question}</p>
-              {quiz.questions[currentQuestion].type === 'single' ? (
+              {quiz.questions[currentQuestion].type === "single" ? (
                 quiz.questions[currentQuestion].options.map((option) => (
                   <div key={option}>
-                    <input type='radio' name='single'   value={option}
+                    <input
+                      type="radio"
+                      name="single"
+                      value={option}
                       checked={selectedValue === option}
-                      onChange={(e)=>setSelectedValue(e.target.value)} />
+                      onChange={(e) => setSelectedValue(e.target.value)}
+                    />
                     <label>{option}</label>
                   </div>
                 ))
-              ) : quiz.questions[currentQuestion].type === 'multiple' ? (
+              ) : quiz.questions[currentQuestion].type === "multiple" ? (
                 quiz.questions[currentQuestion].options.map((option) => (
                   <div key={option}>
-                    <input type='checkbox'     value={option}
+                    <input
+                      type="checkbox"
+                      value={option}
                       name={option}
-                    
-                      onChange={(e)=>setSelectedValue(e.target.value)} />
+                      onChange={(e) => setSelectedValue(e.target.value)}
+                    />
                     <label>{option}</label>
                   </div>
                 ))
-              ) : quiz.questions[currentQuestion].type === 'boolean' ? (
+              ) : quiz.questions[currentQuestion].type === "boolean" ? (
                 <div>
-                  <input type='radio' name='boolean'   value='True'
-                    checked={selectedValue === 'True'}
-                    onChange={(e)=>setSelectedValue(e.target.value)} />
+                  <input
+                    type="radio"
+                    name={`${quiz.questions[currentQuestion].id}-boolean`}
+                    value="True"
+                    onChange={(e) => setSelectedValue(e.target.value)}
+                  />
                   <label>True</label>
-                  <input type='radio' name='boolean'  value='False'
-                    checked={selectedValue === 'False'}
-                    onChange={(e)=>setSelectedValue(e.target.value)} />
+                  <input
+                    type="radio"
+                    name={`${quiz.questions[currentQuestion].id}-boolean`}
+                    value="False"
+                    onChange={(e) => setSelectedValue(e.target.value)}
+                  />
                   <label>False</label>
                 </div>
               ) : null}
@@ -62,6 +74,5 @@ function QuizTest() {
     </div>
   );
 }
-
 
 export { QuizTest };
